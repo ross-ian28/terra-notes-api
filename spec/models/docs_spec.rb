0ccdb7c43aab46d395f7b8665f8cb046c12d061a
Ian Ross <ianrossdrums@gmail.com>
2023-06-24 15:23:02 -0600

    Finish model tests

--- spec/models/docs_spec.rb
+++ spec/models/docs_spec.rb
@@ -3,9 +3,8 @@ require 'shoulda/matchers'
 
 RSpec.describe Doc, type: :model do
   describe "validations" do
-    it { should validate_presence_of :id }
     it { should validate_presence_of :user_id }
-    it { should validate_presence_of :contents }
+    it { should validate_presence_of :password }
   end
   describe "relationships" do
     it { should belong_to(:user) }
