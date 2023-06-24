6e1e25130dd9b6253e7d751920f3fce3f6fd4710
Ian Ross <ianrossdrums@gmail.com>
2023-06-24 15:23:52 -0600

    Update models to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    has_many :notes
+    has_many :docs
+    
     validates_presence_of :email
+    validates_uniqueness_of :email
     validates_presence_of :username
     validates_presence_of :password
     validates_presence_of :logged_in
