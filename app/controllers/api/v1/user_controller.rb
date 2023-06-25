d9b446848ae2a1c8616dda5c55f8eec6d048f181
Ian Ross <ianrossdrums@gmail.com>
2023-06-25 15:58:34 -0600

    Logout tests pass

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UserController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       render json: UserSerializer.user(user), status: 201
     else
       render json: user.errors.full_messages.to_sentence.to_s, status: 400
@@ -25,6 +26,11 @@ class Api::V1::UserController < ApplicationController
     else
       render json: 'Invalid info', status: 400
     end
+  end
+
+  def logout
+    reset_session
+    render json: 'Logged out successfully', status: 201
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       post "/register", to: "user#create" #create/register a user
       get "/user", to: "user#index" #get user information
       post "/login", to: "user#login" #login a user/create a session
+      delete "/logout", to: "user#logout" #logout a user/delete a session
     end
   end
 end
