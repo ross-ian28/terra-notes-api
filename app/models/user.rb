2387aeb9a6810be1c84f85fa618212d8458ba0fb
Ian Ross <ianrossdrums@gmail.com>
2023-07-05 14:15:39 -0600

    Modify models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,7 @@ class User < ApplicationRecord
     has_many :notes
     has_many :docs
     
-    validates_presence_of :email
     validates_uniqueness_of :email
-    validates_presence_of :username
-    validates_presence_of :password
-    validates_presence_of :logged_in
 
     has_secure_password
 end
--- spec/models/notes_spec.rb
+++ spec/models/notes_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 require 'shoulda/matchers'
 
 RSpec.describe Note, type: :model do
-  describe "validations" do
-    it { should validate_presence_of :user_id }
-    it { should validate_presence_of :contents }
-  end
+  # describe "validations" do
+  #   it { should validate_presence_of :user_id }
+  #   it { should validate_presence_of :contents }
+  # end
   describe "relationships" do
     it { should belong_to(:user) }
   end
