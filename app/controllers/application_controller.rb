27e904fc796c6e70d10a090b791c9ac5885c0ef3
Ian Ross <ianrossdrums@gmail.com>
2023-08-03 14:42:23 -0600

    Add cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
-end
+    before_action :cors_preflight_check
+    after_action :cors_set_access_control_headers
+  
+    private
+  
+    def cors_set_access_control_headers
+      response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' # Replace with the URL where your React frontend is running
+      response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, PATCH, DELETE, GET, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Authorization'
+    end
+  
+    def cors_preflight_check
+      if request.method == 'OPTIONS'
+        headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' # Replace with the URL where your React frontend is running
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, PATCH, DELETE, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Authorization'
+        headers['Access-Control-Max-Age'] = '1728000'
+        render plain: '', status: 204
+      end
+    end
+  end
+  
