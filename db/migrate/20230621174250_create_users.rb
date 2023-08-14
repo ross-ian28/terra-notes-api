9e3414f43046baf1cda1ba7536de574476d15026
Ian Ross <ianrossdrums@gmail.com>
2023-08-14 12:18:41 -0600

    Update schema

--- db/migrate/20230621174250_create_users.rb
+++ db/migrate/20230621174250_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :name, null: false
+      t.string :email, null: false
       t.string :username
-      t.string :password_digest
-      t.boolean :logged_in, default: false
-      t.boolean :incognito_mode, default: false
+      t.string :password_digest, null: false
+      t.boolean :logged_in, default: false, null: false
+      t.boolean :incognito_mode, default: false, null: false
 
       t.timestamps
     end
--- db/migrate/20230621174838_create_notes.rb
+++ db/migrate/20230621174838_create_notes.rb
@@ -1,8 +1,8 @@
 class CreateNotes < ActiveRecord::Migration[7.0]
   def change
     create_table :notes do |t|
-      t.references :user, foreign_key: true
-      t.text :contents
+      t.references :user, foreign_key: true, null: false
+      t.text :contents, defaul: "", null: false
 
       t.timestamps
     end
