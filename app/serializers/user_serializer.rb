46bb945bc0389a1039d006dfd89795898e4267d9
Ian Ross <ianrossdrums@gmail.com>
2023-08-14 12:19:29 -0600

    Update serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,26 +1,36 @@
 class UserSerializer
-    def self.get_user(user)
-      {
-        data: {
-          type: "users",
-          id: user.id,
-          attributes: {
-            name: user.name,
-            email: user.email,
-            username: user.username,
-            logged_in: user.logged_in,
-            incognito_mode: user.incognito_mode
-          }
+  def self.get_user(user)
+    {
+      data: {
+        type: "users",
+        id: user.id,
+        attributes: {
+          name: user.name,
+          email: user.email,
+          username: user.username,
+          logged_in: user.logged_in,
+          incognito_mode: user.incognito_mode,
+          notes: user.notes.map do |note|  # Use user.notes instead of just notes
+            {
+              id: note.id,
+              type: "notes",
+              attributes: {
+                user_id: note.user_id,
+                contents: note.contents
+              }
+            }
+          end
         }
       }
-    end
-
-    def self.logout(msg)
-      {
-        data: {
-          type: "users",
-          msg: msg
-        }
-      }
-    end
+    }
   end
+
+  def self.logout(msg)
+    {
+      data: {
+        type: "users",
+        msg: msg
+      }
+    }
+  end
+end
